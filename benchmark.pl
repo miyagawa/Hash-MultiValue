b4db5d357d7f0f0e30e0695baed160b323aa346c
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-12-15 04:30:48 -0800

    tweak benchmark

--- benchmark.pl
+++ benchmark.pl
@@ -3,24 +3,23 @@ use strict;
 use Benchmark;
 use Hash::MultiValue;
 
-my @form = map { $_ => $_ % 5 == 0 ? [ rand 1000, rand 10000 ] :  rand 1000 } 1..200;
+my $how_many = shift || 100;
+
+my @form = map { $_ => $_ % 5 == 0 ? [ rand 1000, rand 10000 ] :  rand 1000 } 1..$how_many;
 
 timethese 0, {
     normal => sub {
         my %form = @form;
-        keys %form;
-        $form{143} = 1000;
-        delete $form{145};
-        my @values = @form{1..100};
+        my @k = keys %form;
+        $form{14} = 1000;
+        delete $form{14};
+        my @values = @form{1..10};
     },
     multivalue => sub {
         my %form = Hash::MultiValue->new(@form);
-        keys %form;
-        $form{143} = 1000;
-        delete $form{145};
-        my @values = @form{1..100};
+        my @k = keys %form;
+        $form{14} = 1000;
+        delete $form{14};
+        my @values = @form{1..10};
     },
 };
-
-
-
